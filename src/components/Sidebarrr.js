f6aeb37c8911d2ccbad1fb014ccb77a72eb3f0a0
SagarSaini17 <145538922+SagarSaini17@users.noreply.github.com>
2023-10-18 13:30:40 +0530

    Update Sidebarrr.js

--- src/components/Sidebarrr.js
+++ src/components/Sidebarrr.js
@@ -10,7 +10,7 @@ export default function Sidebar() {
                                     <path d="m11.42 2 3.428 6-3.428 6H4.58L1.152 8 4.58 2h6.84zM4.58 1a1 1 0 0 0-.868.504l-3.428 6a1 1 0 0 0 0 .992l3.428 6A1 1 0 0 0 4.58 15h6.84a1 1 0 0 0 .868-.504l3.429-6a1 1 0 0 0 0-.992l-3.429-6A1 1 0 0 0 11.42 1H4.58z" />
                                     <path d="M6.848 5.933a2.5 2.5 0 1 0 2.5 4.33 2.5 2.5 0 0 0-2.5-4.33zm-1.78 3.915a3.5 3.5 0 1 1 6.061-3.5 3.5 3.5 0 0 1-6.062 3.5z" />
                                 </svg>
-                    <span class="fs-4">Sidebar</span>
+                    <span class="fs-4">Dashbord</span>
                 </a>
                 <hr/>
                     <ul class="nav nav-pills flex-column mb-auto">
@@ -44,6 +44,13 @@ export default function Sidebar() {
                                 Customers
                             </a>
                         </li>
+                         <li>
+                            <a href="/" class="nav-link text-white">
+                                <svg class="bi me-2" width="16" height="16"><use href="#people-circle"></use></svg>
+                                Help
+                            </a>
+                        </li>
+    
                     </ul>
                     <hr/>
                         <div class="dropdown">
